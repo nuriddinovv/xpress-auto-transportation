4ce9332b054da62c71dce8cea08fa38658cd792c
masharipov <masharipovfarrux@gmail.com>
2025-10-27 16:33:51 +0500

    contacts and blogs

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layout/main-layout";
 import BlogID from "./pages/blog/blog-id";
 import NotFound from "./pages/not-found";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Blog from "./pages/blog/blog";
-import Contacts from "./pages/contacts";
+import About from "./pages/About";
+import Blog from "./pages/blog/Blog";
+import Contacts from "./pages/Contacts";
+import Home from "./pages/Home";
 
 function App() {
   return (
